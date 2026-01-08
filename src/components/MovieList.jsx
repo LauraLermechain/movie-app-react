a244219f7568efcaf1ac727c51d03016ccfe0003
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-01-08 21:45:09 +0100

    feat : ajout des catégories

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,19 +4,26 @@ import { Link } from "react-router-dom";
 
 export default function MovieList() {
     const [movies, setMovies] = useState([]);
+    const [category, setCategory] = useState("popular");
+
 
     useEffect(() => {
         const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
             .then((res) => res.json())
             .then((data) => setMovies(data.results || []));
-    }, []);
+    }, [category]);
 
     return (
         <div>
             <h1>Films populaires</h1>
-
+            <div>
+                <button onClick={() => setCategory("now_playing")}>Now Playing</button>
+                <button onClick={() => setCategory("popular")}>Popular</button>
+                <button onClick={() => setCategory("top_rated")}>Top Rated</button>
+                <button onClick={() => setCategory("upcoming")}>Upcoming</button>
+            </div>
             <div>
                 {movies.map((m) => (
                     <div key={m.id}>
