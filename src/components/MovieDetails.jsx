204be9401a5d74f99dc99876f56cf993ba40c7d2
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-01-08 22:35:37 +0100

    feat : afficher info détaillées dans pageDetail

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,3 +1,26 @@
-export default function MovieDetails() {
-    return <h1>Détails du film</h1>;
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"; // useParams pour récupérer les paramètres de l'URL
+
+export default function MovieDetail() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState(null);
+
+    useEffect(() => {
+        const apiKey = import.meta.env.VITE_TMDB_API_KEY;
+
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+            .then((res) => res.json())
+            .then((data) => setMovie(data));
+    }, [id]);
+
+    if (!movie) return <p>Chargement...</p>;
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <p><strong>Résumé :</strong> {movie.overview}</p>
+            <p><strong>Date de sortie :</strong> {movie.release_date}</p>
+            <p><strong>Note moyenne :</strong> {movie.vote_average}</p>
+        </div>
+    );
 }
