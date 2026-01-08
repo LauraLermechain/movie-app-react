089659b9f6ccba01ca142fd0c25fd25f2e937de6
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-01-08 22:07:14 +0100

    feat : barre de recherche film

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,15 +5,22 @@ import { Link } from "react-router-dom";
 export default function MovieList() {
     const [movies, setMovies] = useState([]);
     const [category, setCategory] = useState("popular");
-
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
-        fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
+        let url = `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`;
+
+        if (search.trim() !== "") {
+            url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=fr-FR&query=${encodeURIComponent(search)}`;
+        }
+
+        fetch(url)
             .then((res) => res.json())
             .then((data) => setMovies(data.results || []));
-    }, [category]);
+
+    }, [category, search]);
 
     return (
         <div>
@@ -24,6 +31,12 @@ export default function MovieList() {
                 <button onClick={() => setCategory("top_rated")}>Top Rated</button>
                 <button onClick={() => setCategory("upcoming")}>Upcoming</button>
             </div>
+            <input
+                type="text"
+                placeholder="Rechercher un film..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
             <div>
                 {movies.map((m) => (
                     <div key={m.id}>
