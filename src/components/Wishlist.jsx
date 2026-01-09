e032487e81e10a27bb7aee9ca9d5f5a9065cae10
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-01-09 11:01:30 +0100

    feat : recherche dans la wishList

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,19 +1,32 @@
 import { useContext } from "react";
 import { WishlistContext } from "../context/WishlistProvider";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Wishlist() {
     const { wishlist, removeFromWishlist } = useContext(WishlistContext);
+    const [search, setSearch] = useState("");
+
 
     if (wishlist.length === 0) {
         return <p>Votre wishlist est vide.</p>;
     }
 
+    const filteredWishlist = wishlist.filter((movie) =>
+        movie.title.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <div>
             <h1>Ma wishlist</h1>
+            <input
+                type="text"
+                placeholder="Rechercher dans la wishlist..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
 
-            {wishlist.map((movie) => (
+            {filteredWishlist.map((movie) => (
                 <div key={movie.id}>
                     <h3>{movie.title}</h3>
 
